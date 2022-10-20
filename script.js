13ccb1a0c84d70bf6dd40e866cd6df64d4bfc838
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-20 12:00:16 -0600

    manipulating Dom with JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote(){
     // Pick a random quote from apiQuotes array'
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace it with 'Unknown'
+    if (!quote.author){ 
+    authorText.textContent = 'Unknown';
+    } else {
+       authorText.textContent = quote.author; 
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
+    
 }
 
 // Get Quotes From API
@@ -20,6 +41,15 @@ async function getQuotes() {
 }
 
 
-// On load
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
 
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On load
 getQuotes();
