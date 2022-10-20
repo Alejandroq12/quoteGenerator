ad00f1ebf738b1e76e0d15fb4b2bf8797915f40f
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-20 12:57:21 -0600

    Finishing project

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 
 let apiQuotes = [];
 
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
 // Show New Quote
 function newQuote(){
+    loading();
     // Pick a random quote from apiQuotes array'
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if author field is blank and replace it with 'Unknown'
@@ -23,13 +39,15 @@ function newQuote(){
     } else {
         quoteText.classList.remove('long-quote');
     }
-
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
     
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl='https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
